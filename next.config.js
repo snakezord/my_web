4773bf57356cdba65d3809f7065e04cb66d39fa2
Roman Zhydyk <nsake@hotmail.com>
2022-07-22 20:55:57 +0100

    Fixing .env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    env: {NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}
 }
 
 module.exports = nextConfig
