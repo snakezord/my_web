875a9828f536a1ef67dbb6a12f6f76a55b352761
Roman Zhydyk <nsake@hotmail.com>
2022-07-25 19:20:08 +0100

    Statue text change on route.

--- components/scene.tsx
+++ components/scene.tsx
@@ -5,6 +5,7 @@ import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 import {CircularProgress, useColorModeValue, useMediaQuery} from "@chakra-ui/react";
 import Typewriter, {TypewriterClass} from 'typewriter-effect';
 import {AnimationClip, Group} from "three";
+import {useRouter} from "next/router";
 
 function Loader() {
 	const {progress} = useProgress()
@@ -17,18 +18,19 @@ function Loader() {
 
 const Scene = () => {
 	const {scene, animations} = useLoader(GLTFLoader, "./scene.glb");
+	const {asPath} = useRouter()
 	return (
 		<Canvas>
 			<ambientLight intensity={useColorModeValue(1, 0)}/>
 			<Suspense fallback={<Loader/>}>
-				<Model scene={scene} animations={animations}/>
+				<Model scene={scene} animations={animations} asPath={asPath}/>
 			</Suspense>
 			<OrbitControls enableZoom={false}/>
 		</Canvas>
 	);
 };
 
-const Model: FC<{ scene: Group, animations: AnimationClip[] }> = ({scene, animations}) => {
+const Model: FC<{ scene: Group, animations: AnimationClip[], asPath: string }> = ({scene, animations, asPath}) => {
 	const group = useRef<any>(null);
 	const {actions} = useAnimations(animations, group);
 	const [isLargerThan1280] = useMediaQuery('(min-width: 1280px)')
@@ -43,15 +45,26 @@ const Model: FC<{ scene: Group, animations: AnimationClip[] }> = ({scene, animat
 				<primitive object={scene}/>
 			</mesh>
 			<Html center position={[-1.1, isLargerThan1280 ? 1 : -1, 0]} id={undefined}>
-				<Typewriter
-					onInit={(typewriter: TypewriterClass) => {
-						typewriter
-							.typeString('Welcome!')
-							.pauseFor(2500)
-							.pause()
-							.start();
-					}}
-				/>
+				<div style={{minWidth: '250px', marginLeft: '25%'}}>
+					{asPath === '/' && <Typewriter
+              options={{delay: 50}}
+              onInit={(typewriter: TypewriterClass) => {
+								typewriter
+									.typeString('Greetings visitor!')
+									.pause()
+									.start();
+							}}
+          />}
+					{asPath === '/works' && <Typewriter
+              options={{delay: 50}}
+              onInit={(typewriter: TypewriterClass) => {
+								typewriter
+									.typeString('Check out my latest work!')
+									.pause()
+									.start();
+							}}
+          />}
+				</div>
 			</Html>
 		</group>
 	)
