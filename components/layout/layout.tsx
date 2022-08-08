0fe1e2744c7550ebe673eae01b504c7963db277f
Roman Zhydyk <nsake@hotmail.com>
2022-08-08 17:30:16 +0100

    Changed photo position

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,86 +1,86 @@
-import {Box, Container, Grid, GridItem, Spacer, Spinner, useColorMode} from "@chakra-ui/react";
-import {FC, PropsWithChildren} from "react";
+import { Box, Container, Grid, GridItem, Spacer, Spinner, useColorMode } from "@chakra-ui/react";
+import { FC, PropsWithChildren } from "react";
 import dynamic from "next/dynamic";
-import {Header} from "../header";
-import {useMediaQuery} from '@chakra-ui/react'
+import { Header } from "../header";
+import { useMediaQuery } from '@chakra-ui/react';
 import Footer from "../footer";
 import NextNProgress from "nextjs-progressbar";
-import {useGLTF} from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
 import ScrollDownSvg from "../scrollDownSvg";
 import Head from "next/head";
 
-useGLTF.preload('/scene.glb')
+useGLTF.preload('/scene.glb');
 
 const Scene = dynamic(() => import('../scene'), {
-	ssr: false,
-	loading: () => {
-		return <Spinner w={'64px'} h={'64px'} emptyColor='#F9248FB3'/>
-	}
+  ssr: false,
+  loading: () => {
+    return <Spinner w={'64px'} h={'64px'} emptyColor='#F9248FB3' />;
+  }
 });
 
-const Layout: FC<PropsWithChildren> = ({children}) => {
-	const {colorMode} = useColorMode();
-	const [isSmallerThan1280] = useMediaQuery('(max-width: 1280px)')
+const Layout: FC<PropsWithChildren> = ({ children }) => {
+  const { colorMode } = useColorMode();
+  const [isSmallerThan1280] = useMediaQuery('(max-width: 1280px)');
 
-	const posPropsGrid1 = isSmallerThan1280
-		? {
-			colStart: 1,
-			colEnd: 13
-		}
-		: {
-			colStart: 3,
-			colEnd: 8
-		};
-	const posPropsGrid2 = isSmallerThan1280
-		? {
-			colStart: 2,
-			colEnd: 12,
-			gridRow: 1,
-			h: '300px'
-		}
-		: {
-			colStart: 8,
-			colEnd: 12,
-			h: '70vh',
-			position: 'fixed',
-			right: '20px',
-			top: '120px',
-			mr: '200px',
-			w: '30%',
-		};
+  const posPropsGrid1 = isSmallerThan1280
+    ? {
+      colStart: 1,
+      colEnd: 13
+    }
+    : {
+      colStart: 3,
+      colEnd: 8
+    };
+  const posPropsGrid2 = isSmallerThan1280
+    ? {
+      colStart: 2,
+      colEnd: 12,
+      gridRow: 1,
+      h: '300px'
+    }
+    : {
+      colStart: 8,
+      colEnd: 12,
+      h: '70vh',
+      position: 'fixed',
+      right: '20px',
+      top: '120px',
+      mr: '200px',
+      w: '30%',
+    };
 
-	return (
-		<Box w='100%' minH='100vh' h='100%' bg={`${colorMode}.brand.100`}>
-			<Head>
-				<link rel='icon' href='/favicon.ico'/>
-			</Head>
-			<Header/>
-			<NextNProgress height={3} color={'#F9248FB3'}/>
-			<ScrollDownSvg/>
-			<Grid templateColumns={'repeat(12, 1fr)'} pt={'5%'} pb={'2%'}>
-				<GridItem
-					{...posPropsGrid1}
-					pt={'5%'}
-					zIndex={1}
-				>
-					<Container maxW={'container.md'}>
-						{children}
-					</Container>
-				</GridItem>
-				{/*@ts-ignore*/}
-				<GridItem
-					{...posPropsGrid2}
-					display={'flex'} alignItems={'center'} justifyContent={'center'}
-					css={{cursor: 'grab'}}
-					zIndex={0}
-				>
-					<Scene/>
-				</GridItem>
-			</Grid>
-			<Spacer/>
-			<Footer/>
-		</Box>
-	)
-}
+  return (
+    <Box w='100%' minH='100vh' h='100%' bg={`${colorMode}.brand.100`}>
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Header />
+      <NextNProgress height={3} color={'#F9248FB3'} />
+      <ScrollDownSvg />
+      <Grid templateColumns={'repeat(12, 1fr)'} pt={'5%'} pb={'2%'}>
+        <GridItem
+          {...posPropsGrid1}
+          pt={'2%'}
+          zIndex={1}
+        >
+          <Container maxW={'container.md'}>
+            {children}
+          </Container>
+        </GridItem>
+        {/*@ts-ignore*/}
+        <GridItem
+          {...posPropsGrid2}
+          display={'flex'} alignItems={'center'} justifyContent={'center'}
+          css={{ cursor: 'grab' }}
+          zIndex={0}
+        >
+          <Scene />
+        </GridItem>
+      </Grid>
+      <Spacer />
+      <Footer />
+    </Box>
+  );
+};
 
 export default Layout;
--- components/me.tsx
+++ components/me.tsx
@@ -1,40 +1,40 @@
-import {Box, Text, chakra, Stack, VStack, useMediaQuery} from "@chakra-ui/react";
-import {FC} from "react";
+import { Box, Text, chakra, Stack, VStack, useMediaQuery } from "@chakra-ui/react";
+import { FC } from "react";
 import Image from 'next/image';
 
 export const ChakraImage = chakra(Image, {
-	shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
+  shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
 });
 
 const Me: FC = () => {
-	const [isLargerThan600] = useMediaQuery('(min-width: 600px)')
-	return (
-		<Stack direction={['column', 'row']} align={['stretch', 'center']} spacing={isLargerThan600 ? 7 : 2}>
-			<Box
-				w={'120px'}
-				h={'120px'}
-				borderWidth={1} borderStyle="solid" borderColor='#ffff' borderRadius={'full'}
-				boxShadow='rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;'
-				alignSelf={'center'}
-			>
-				<ChakraImage src={'/me_square.jpeg'} alt='me' borderRadius={'full'} width="120px" height="120px"/>
-			</Box>
-			<VStack align='flex-start' spacing={1}>
-				<Text fontSize={'4xl'} fontWeight={'bold'}>Roman Zhydyk</Text>
-				<Stack direction={['column', 'row']} spacing={0} fontSize={isLargerThan600 ? 'md' : 'sm'}>
-					<Text pr={2}>
-						Software Engineer.
-					</Text>
-					<Text pr={2}>
-						Crypto Enthusiast.
-					</Text>
-					<Text pr={2}>
-						Truth Seeker.
-					</Text>
-				</Stack>
-			</VStack>
-		</Stack>
-	);
+  const [isLargerThan600] = useMediaQuery('(min-width: 600px)');
+  return (
+    <Stack direction={['column-reverse', 'row']} align={['stretch', 'center']} spacing={isLargerThan600 ? 7 : 2}>
+      <VStack align='flex-start' spacing={1}>
+        <Text fontSize={'4xl'} fontWeight={'bold'}>Roman Zhydyk</Text>
+        <Stack direction={['column', 'row']} spacing={0} fontSize={isLargerThan600 ? 'md' : 'sm'}>
+          <Text pr={2}>
+            Software Engineer.
+          </Text>
+          <Text pr={2}>
+            Crypto Enthusiast.
+          </Text>
+          <Text pr={2}>
+            Truth Seeker.
+          </Text>
+        </Stack>
+      </VStack>
+      <Box
+        w={'140px'}
+        h={'140px'}
+        borderWidth={1} borderStyle="solid" borderColor='#ffff' borderRadius={'full'}
+        boxShadow='rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;'
+        alignSelf={'center'}
+      >
+        <ChakraImage src={'/me_square.jpeg'} alt='me' borderRadius={'full'} width="140px" height="140px" />
+      </Box>
+    </Stack>
+  );
 };
 
 export default Me;
