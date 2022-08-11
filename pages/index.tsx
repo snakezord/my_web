763f023e117c57511261c5ced07607635efe7560
Roman Zhydyk <nsake@hotmail.com>
2022-08-11 21:22:24 +0100

    Improved blog posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,43 @@
-import type {NextPage} from 'next';
-import {useMediaQuery, VStack} from '@chakra-ui/react';
+import type { NextPage } from 'next';
+import { useMediaQuery, VStack } from '@chakra-ui/react';
 import Me from '../components/me';
 import Whoami from "../components/whoami";
-import {Social} from "../components/social";
+import { Social } from "../components/social";
 import Animate from "../components/animate";
 import Hobbies from "../components/hobbies";
 import Blog from "../components/blog";
 import Seo from "../components/seo";
 
 const Home: NextPage = () => {
-	const [isLargerThan600] = useMediaQuery('(min-width: 600px)')
-	return (
-		<>
-			<Seo
-				metaTitle={"Roman Zhydyk's website & blog"}
-				metaDescription={"Roman Zhydyk's personal website & blog. " +
-					"Roman is a software engineer with a diverse skill-set. " +
-					"Like a real man, he constantly seeks improvement—both physically & mentally."
-				}
-			/>
-			<VStack spacing={isLargerThan600 ? 12 : 6} align={'flex-start'}>
-				<Animate>
-					<Me/>
-				</Animate>
-				<Animate delay={0.2}>
-					<Whoami/>
-				</Animate>
-				<Animate delay={0.4}>
-					<Hobbies/>
-				</Animate>
-				<Animate delay={0.6}>
-					<Blog/>
-				</Animate>
-				<Animate delay={0.6}>
-					<Social/>
-				</Animate>
-			</VStack>
-		</>
-	);
+  const [isLargerThan600] = useMediaQuery('(min-width: 600px)');
+  return (
+    <>
+      <Seo
+        metaTitle={"Roman Zhydyk's website & blog"}
+        metaDescription={"Roman Zhydyk's personal website & blog. " +
+          "Roman is a software engineer with a diverse skill-set. " +
+          "Like a real man, he constantly seeks improvement—both physically & mentally."
+        }
+      />
+      <VStack spacing={isLargerThan600 ? 12 : 6} align={'flex-start'}>
+        <Animate>
+          <Me />
+        </Animate>
+        <Animate delay={0.2}>
+          <Whoami />
+        </Animate>
+        <Animate delay={0.4}>
+          <Hobbies />
+        </Animate>
+        <Animate delay={0.6}>
+          <Blog />
+        </Animate>
+        <Animate delay={0.8}>
+          <Social />
+        </Animate>
+      </VStack>
+    </>
+  );
 };
 
 export default Home;
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,74 +1,76 @@
-import {ApolloClient, gql, InMemoryCache} from "@apollo/client";
-import {getImageUrl, server} from "../../config";
-import {AspectRatio, Box, Heading, HStack, Text, VStack} from "@chakra-ui/react";
-import ReactMarkdown from 'react-markdown'
+import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
+import { getImageUrl, server } from "../../config";
+import { AspectRatio, Box, Heading, HStack, Text, VStack } from "@chakra-ui/react";
+import ReactMarkdown from 'react-markdown';
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
-import {ChakraImage} from "../../components/me";
+import { ChakraImage } from "../../components/me";
 import Moment from "react-moment";
 import Seo from "../../components/seo";
+import Link from "next/link";
 
 const Post = (props: any) => {
-	return (
-		<>
-			<Seo
-				metaTitle={props.article.attributes.title}
-				metaDescription={props.article.attributes.description}
-				shareImage={getImageUrl(props.article.attributes.image.data.attributes.url)}
-				article
-			/>
-			<VStack align={'stretch'} spacing={3}>
-				<VStack align={'flex-start'} pb={5}>
-					<Text color={'GrayText'} casing={'capitalize'}>
-						{props.article.attributes.category.data.attributes.name}
-					</Text>
-					<HStack>
-						<Text fontSize={'xx-small'} fontWeight={'light'}>
-							<Moment format="MMM D, Y">{props.article.attributes.createdAt}</Moment>
-						</Text>
-						<Text fontSize={'xx-small'} fontWeight={'light'}>
-							{props.readingTime.text}
-						</Text>
-					</HStack>
-					<Heading>{props.article.attributes.title}</Heading>
-					<Text color={'GrayText'}>{props.article.attributes.description}</Text>
-				</VStack>
-				<ChakraImage
-					src={getImageUrl(props.article.attributes.image.data.attributes.url)}
-					alt={props.article.attributes.image.data.attributes.alternativeText}
-					width={props.article.attributes.image.data.attributes.width}
-					height={props.article.attributes.image.data.attributes.height}
-				/>
-				<Box as={'article'}>
-					<ReactMarkdown
-						components={ChakraUIRenderer(
-							{
-								iframe: (props) => {
-									return <AspectRatio ratio={16 / 9} my={5}>
-										<iframe {...props}/>
-									</AspectRatio>
-								}
-							})}
-						rehypePlugins={[rehypeRaw]}
-						remarkPlugins={[remarkGfm]}
-					>
-						{props.article.attributes.content}
-					</ReactMarkdown>
-				</Box>
-			</VStack>
-		</>
-	)
-}
+  return (
+    <>
+      <Seo
+        metaTitle={props.article.attributes.title}
+        metaDescription={props.article.attributes.description}
+        shareImage={getImageUrl(props.article.attributes.image.data.attributes.url)}
+        article
+      />
+      <VStack align={'stretch'} spacing={3}>
+        <VStack align={'flex-start'} pb={5}>
+          <Text color={'GrayText'} casing={'capitalize'}>
+            {props.article.attributes.category.data.attributes.name}
+          </Text>
+          <HStack>
+            <Text fontSize={'xx-small'} fontWeight={'light'}>
+              <Moment format="MMM D, Y">{props.article.attributes.createdAt}</Moment>
+            </Text>
+            <Text fontSize={'xx-small'} fontWeight={'light'}>
+              {props.readingTime.text}
+            </Text>
+          </HStack>
+          <Heading>{props.article.attributes.title}</Heading>
+          <Text color={'GrayText'}>{props.article.attributes.description}</Text>
+        </VStack>
+        <ChakraImage
+          src={getImageUrl(props.article.attributes.image.data.attributes.url)}
+          alt={props.article.attributes.image.data.attributes.alternativeText}
+          width={props.article.attributes.image.data.attributes.width}
+          height={props.article.attributes.image.data.attributes.height}
+        />
+        <Box as={'article'}>
+          <ReactMarkdown
+            components={ChakraUIRenderer(
+              {
+                iframe: (props) => {
+                  return <AspectRatio ratio={16 / 9} my={5}>
+                    <iframe {...props} />
+                  </AspectRatio>;
+                },
+                a: (props) => <a {...props} contentEditable="false" target={'_blank'} rel="noreferrer" style={{ background: '#FFD700', color: 'black' }} />
+              })}
+            rehypePlugins={[rehypeRaw]}
+            remarkPlugins={[remarkGfm]}
+          >
+            {props.article.attributes.content}
+          </ReactMarkdown>
+        </Box>
+      </VStack>
+    </>
+  );
+};
 
 export async function getStaticProps(path: any) {
-	const readingTime = require('reading-time');
-	const client = new ApolloClient({
-		uri: `${server}/graphql`,
-		cache: new InMemoryCache()
-	})
-	const {data} = await client.query({
-		query: gql`
+  const readingTime = require('reading-time');
+  const client = new ApolloClient({
+    uri: `${server}/graphql`,
+    cache: new InMemoryCache()
+  });
+  const { data } = await client.query({
+    query: gql`
 			query {
 				articles(filters:{slug: {eq: "${path.params.slug}"}}) {
 					data {
@@ -100,19 +102,19 @@ export async function getStaticProps(path: any) {
 				}
 			}
 		`
-	});
-	const stats = readingTime(data.articles.data[0].attributes.content);
-	return {props: {article: data.articles.data[0], readingTime: stats}, revalidate: 1}
+  });
+  const stats = readingTime(data.articles.data[0].attributes.content);
+  return { props: { article: data.articles.data[0], readingTime: stats }, revalidate: 1 };
 }
 
 export async function getStaticPaths() {
-	const client = new ApolloClient({
-		uri: `${server}/graphql`,
-		cache: new InMemoryCache()
-	})
+  const client = new ApolloClient({
+    uri: `${server}/graphql`,
+    cache: new InMemoryCache()
+  });
 
-	const {data} = await client.query({
-		query: gql`
+  const { data } = await client.query({
+    query: gql`
 			query {
 				articles {
 					data {
@@ -124,15 +126,15 @@ export async function getStaticPaths() {
 				}
 			}
 		`
-	});
-	return {
-		paths: data.articles.data.map((article: any) => ({
-			params: {
-				slug: article.attributes.slug,
-			}
-		})),
-		fallback: false
-	}
+  });
+  return {
+    paths: data.articles.data.map((article: any) => ({
+      params: {
+        slug: article.attributes.slug,
+      }
+    })),
+    fallback: false
+  };
 }
 
 export default Post;
