5e23c937924b4f720446b316085bf0b38cd96afa
Roman Zhydyk <nsake@hotmail.com>
2023-02-11 17:37:18 +0000

    Improved work card & slider

--- components/slider.tsx
+++ components/slider.tsx
@@ -1,26 +1,30 @@
 import "react-image-gallery/styles/css/image-gallery.css";
-import { FC, useMemo } from "react";
-import { Box } from "@chakra-ui/react";
+import { FC, useMemo, useState } from "react";
+import { Box, Image, useColorMode } from "@chakra-ui/react";
 import ImageGallery, { ReactImageGalleryItem } from 'react-image-gallery';
 
 export const Slider: FC<{ images: string[]; }> = ({ images }) => {
+  const { colorMode } = useColorMode();
+  const [isFullScreen, setIsFullScreen] = useState(false);
   const items = useMemo(() => images.reduce((arr, el) => {
     return [...arr, {
       original: el,
     }];
   }, [] as ReactImageGalleryItem[]), [images]);
-
+  console.log(isFullScreen);
+  
   return (
     <Box
       minWidth='300px'
-      minHeight='230px'
       w={'100%'}
       h={'100%'}
       borderRadius={'10px'}
-      p={1}
+      border={`1px solid ${colorMode === 'dark' ? '#FFD70080' : '#FFD700CC'}`}
     >
       <ImageGallery
         lazyLoad={true}
+        renderItem={(item) => <Image src={item.original} loading="lazy" alt={item.original} height={'auto'} maxHeight={isFullScreen ? 'calc(100vh - 80px)' : ['186px', '380px']} width='100%' objectFit={isFullScreen ? 'contain' : "cover"} objectPosition='0 0' borderRadius='10px' />}
+        onScreenChange={(isFS) => setIsFullScreen(isFS)}
         items={items}
         showThumbnails={false}
         showPlayButton={false}
